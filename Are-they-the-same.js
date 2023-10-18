273b122cdabb10033bebe7e7554c362f6df8d44e
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-18 15:35:13 -0700

    Update Are-they-the-same.js

--- Are-they-the-same.js
+++ Are-they-the-same.js
@@ -40,3 +40,8 @@ function comp(array1, array2) {
 }
 
 // or
+
+function comp(a, b) {
+  if (!a || !b || a.length !== b.length) return false;
+  return a.map(x => x * x).sort().toString() === b.sort().toString();
+}
